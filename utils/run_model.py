8874c1f6710af56c1c37505548966521f00620e2
GonzaloZelinka <gonzalozelinka@gmail.com>
2023-04-14 13:36:25 -0300

    improving run_model

--- utils/run_model.py
+++ utils/run_model.py
@@ -8,7 +8,11 @@ from presidio_analyzer.nlp_engine import NlpArtifacts
 from tqdm import tqdm
 
 from Config import Config
-from transformers_rec import Analyzer, Anonymizer
+import os
+import sys
+import warnings
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from transformers_rec import (Analyzer, Anonymizer)
 
 
 def create_obj(an_r, text):
@@ -27,45 +31,39 @@ def create_obj(an_r, text):
 
 def model_results(input_path, output_path):
     analyzer = Analyzer("obi/deid_roberta_i2b2") # "en_core_web_lg" or "obi/deid_roberta_i2b2"
-    analyzer_engine = analyzer.get_engine()
-
-    final_result = []
 
     # get the total number of rows in the CSV file
     total_rows = pd.read_csv(input_path, usecols=[0]).shape[0]
     print(f"getting results for the ner model and presidio. Progressively writing the results in {output_path}...")
     progress_bar = tqdm(total=total_rows)
-    for batch in pd.read_csv(input_path, encoding="ISO-8859-1", chunksize=Config.BATCH_SIZE):
-      with open(output_path, "a", encoding="ISO-8859-1") as fp:
-        writer = csv.writer(fp)
-        # Write the header row
-        writer.writerow(['SITE_URL', 'ITEM_GROUP_ID', 'PVID', 'TEXT', 'ENTITIES'])
-        # Process the texts as batch for improved performance
-        nlp_artifacts_batch: Iterator[
-            Tuple[str, NlpArtifacts]
-        ] = analyzer_engine.nlp_engine.process_batch(
-            texts=batch, language="en"
-        )
+    with open(output_path, "a", encoding="ISO-8859-1") as fp:
+      writer = csv.writer(fp)
+       # Write the header row
+      writer.writerow(['SITE_URL', 'ITEM_GROUP_ID', 'PVID', 'TEXT', 'ENTITIES'])
+      anonymizer = Anonymizer()
 
-        anonymizer = Anonymizer()
-        # for i, (text, nlp_artifacts) in tqdm(enumerate(nlp_artifacts_batch), total=len(data_list)):
-        for i, (text, nlp_artifacts) in enumerate(nlp_artifacts_batch):
-          new_text = text.split('[,]')
+      for batch in pd.read_csv(input_path, encoding="ISO-8859-1", chunksize=Config.BATCH_SIZE, names=Config.columns, header=0):
+         for row in batch.iterrows():
+          site_url = row[1]['SITE_URL']
+          item_group_id = row[1]['ITEM_GROUP_ID']
+          pvids = row[1]['PVID']
+          review = row[1]['REVIEW']
+
           results = analyzer.analyze(
-            text=str(new_text[3]), nlp_artifacts=nlp_artifacts, language="en", entities=Config.entities, score_threshold=Config.threshold
+            text=str(review), language="en", entities=Config.entities, score_threshold=Config.threshold
           )
           result = []
           if Config.check_overlaps:
-                text_anon = anonymizer.anonymize(new_text[3], results)
+                text_anon = anonymizer.anonymize(str(review), results)
                 text_anon = sorted(text_anon.items, key=lambda x: x.start)
-                for i, res in enumerate(text_anon):
+                for res in text_anon:
                     result.append({"start": res.start, "end": res.end, "entity": res.entity_type, "text": res.text})
           else:
-              result = create_obj(result, new_text[3])
-          row = [new_text[0], new_text[1], eval(new_text[2]), new_text[3], json.dumps(result)]
-          # final_result = {"SITE_URL": new_text[0], "ITEM_GROUP_ID":new_text[1], "PVID": eval(new_text[2]), "TEXT": text[3], "ENTITIES": result}
+              result = create_obj(result, review)
+          row = [site_url, item_group_id, eval(pvids), review, json.dumps(result)]
           writer.writerow(row)
           progress_bar.update(1)
-      # fp=open(output_path,'a', encoding="ISO-8859-1") # output file
-      # json.dump(final_result, fp) 
-    print("Done!") 
+    print("\nDone!") 
+
+warnings.filterwarnings("ignore", message="You seem to be using the pipelines sequentially on GPU.")
+model_results(input_path="testing-data/cleaned_sentences_from_db2.csv", output_path="testing-data/output_from_db2.csv")
